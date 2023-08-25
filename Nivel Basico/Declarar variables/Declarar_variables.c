dbfc4af89b649dc099b02625451e4247c50908bf
sValderas1997 <135936397+sValderas1997@users.noreply.github.com>
2023-08-25 17:51:47 -0400

    Add files via upload

---
+++ Nivel Basico/Declarar variables/Declarar_variables.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+
+int datoA,datoB;
+
+int main()
+{
+    datoA=-23;
+    datoB=6;
+    printf("Programacion Grafica 2020");
+    printf("\nDatoA= %d  DatoB= %d    \n",datoA, datoB);
+}
